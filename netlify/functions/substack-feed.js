e3f81295c70825ca5961d40e03971d66c5604aaa
Justin Reed <j.reed715@gmail.com>
2026-03-12 03:20:13 -0400

    removed parser

--- netlify/functions/substack-feed.js
+++ netlify/functions/substack-feed.js
@@ -4,13 +4,11 @@ const FEED_URL = "https://chelseafarmerssupply.substack.com/feed";
 exports.handler = async () => {
   const res = await fetch(FEED_URL);
   const xml = await res.text();
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(xml, "text/xml");
 
   return {
     statusCode: res.status,
     headers: {
-      "content-type": "text/xml; charset=utf-8",
+      "content-type": "application/xml; charset=utf-8",
     },
     body: xml,
   };

077b6141100bdde9ff18acabc30299ece97bd3d1
Justin Reed <j.reed715@gmail.com>
2026-03-12 03:18:33 -0400

    developing netlify worker

--- netlify/functions/substack-feed.js
+++ netlify/functions/substack-feed.js
@@ -4,11 +4,13 @@ const FEED_URL = "https://chelseafarmerssupply.substack.com/feed";
 exports.handler = async () => {
   const res = await fetch(FEED_URL);
   const xml = await res.text();
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xml, "text/xml");
 
   return {
     statusCode: res.status,
     headers: {
-      "content-type": "application/xml; charset=utf-8",
+      "content-type": "text/xml; charset=utf-8",
     },
     body: xml,
   };
